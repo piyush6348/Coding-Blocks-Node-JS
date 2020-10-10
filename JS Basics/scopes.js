63e481bc3e8ebaac2f5f61cce7f677e521b5a815
piyush6348 <piyush6348@gmail.com>
2020-10-10 14:45:06 +0530

    added js basics var let const

--- JS Basics/scopes.js
+++ JS Basics/scopes.js
@@ -1,6 +1,9 @@
+console.log("\n\n\n------------------Scopes---------------\n\n");
+
 // Global scope, Function scope and Block scope.
 // var: Function scope.
 // let: Block scope.
+// const: Block scope.
 
 
 function someMethod() {
@@ -25,3 +28,62 @@ function someMethod() {
     }
     console.log(c);
 }
+
+
+const x = 7;
+// x = 0; error
+
+const arr = [1, 2, 3]
+arr.push(10) // Works
+// arr = [1] Not allowed
+
+/*
+    -> If a variable is const then it's value cannot be changed(For primitive types eg Boolean, String, Number).
+    -> But if a non primitive data is allocated to a const variable eg arrays, objects etc then their 
+    reference cannot be changed but the object itself can be changed.
+
+    In above eg modifying the data of reference worked( arr.push)
+    But modifying the reference failed(arr = ..)
+*/
+
+console.log("\n\n\n------------------Scopes More examples---------------\n\n");
+
+var myVar = 11;
+
+function myVarFunc() {
+    var myVar = 21;
+    if (true) {
+        var myVar = 31;
+        console.log(myVar); // 31
+    }
+    console.log(myVar); // 31
+}
+myVarFunc()
+console.log(myVar); // 11
+
+
+let myLet = 11;
+
+function myLetFunc() {
+    let myLet = 21;
+    if (true) {
+        let myLet = 31;
+        console.log(myLet); // 31
+    }
+    console.log(myLet); // 21
+}
+myLetFunc()
+console.log(myLet); // 11
+
+let myConst = 11;
+
+function myConstFunc() {
+    let myConst = 21;
+    if (true) {
+        let myConst = 31;
+        console.log(myConst); // 31
+    }
+    console.log(myConst); // 21
+}
+myConstFunc()
+console.log(myConst); // 11
