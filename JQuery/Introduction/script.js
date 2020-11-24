36e151d213baea429b302bd15b259736a391c4ce
piyush6348 <piyush6348@gmail.com>
2020-11-25 00:28:29 +0530

    added JQuery Intro

---
+++ JQuery/Introduction/script.js
@@ -0,0 +1,12 @@
+// $ is the function that represents jQuery. We can also write jQuery everytime
+// but this is kinda shortform.
+$()
+
+// All div elements.
+$('div')
+
+// All div elements with class = 'c1'
+$('div.c1')
+
+// Elements having ID = 'id1'
+$('#id1')
